07fe93add169ab1cfc1ed161202ea7b560baa471
xeeshan <info.xeeshan@gmail.com>
2021-02-08 11:05:21 +0500

    update fetching results

--- app/src/main/java/com/fyp/fragments/FragmentUpdate.kt
+++ app/src/main/java/com/fyp/fragments/FragmentUpdate.kt
@@ -4,11 +4,11 @@ import android.app.DatePickerDialog
 import android.app.ProgressDialog
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.fyp.R
@@ -16,30 +16,28 @@ import com.fyp.activities.ActivityDashboard
 import com.fyp.interfaces.iOnBackPressed
 import com.fyp.utils.Constant
 import com.fyp.utils.SessionManager
-import com.fyp.utils.Util
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_signup.*
-import kotlinx.android.synthetic.main.fragment_signup.confirmEPassword
 import kotlinx.android.synthetic.main.fragment_signup.dateOfBirthTv
 import kotlinx.android.synthetic.main.fragment_signup.etPassword
 import kotlinx.android.synthetic.main.fragment_signup.firstNameTv
 import kotlinx.android.synthetic.main.fragment_signup.lastNameTv
 import kotlinx.android.synthetic.main.fragment_signup.mobileTv
 import kotlinx.android.synthetic.main.fragment_update.*
-import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 
-class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
+
+class FragmentUpdate : Fragment() ,iOnBackPressed, View.OnClickListener {
     val myCalendar = Calendar.getInstance()
     private var mDatabase: DatabaseReference? = null
     private var sessionManager: SessionManager? = null
+    var firebaseAuth = FirebaseAuth.getInstance()
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
@@ -49,9 +47,11 @@ class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
         super.onViewCreated(view, savedInstanceState)
         init()
         openDateDialog()
+        getDataFromFirebase()
     }
     fun init(){
         updateBtn.setOnClickListener(this)
+        firebaseAuth = FirebaseAuth.getInstance()
         sessionManager = SessionManager(activity as ActivityDashboard)
     }
 
@@ -60,12 +60,12 @@ class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
             var datePicker =
                 activity?.let {
                     DatePickerDialog(
-                        it,
-                        R.style.DialogTheme,
-                        date,
-                        myCalendar[Calendar.YEAR],
-                        myCalendar[Calendar.MONTH],
-                        myCalendar[Calendar.DAY_OF_MONTH]
+                            it,
+                            R.style.DialogTheme,
+                            date,
+                            myCalendar[Calendar.YEAR],
+                            myCalendar[Calendar.MONTH],
+                            myCalendar[Calendar.DAY_OF_MONTH]
                     )
                 }
 //            datePicker?.getDatePicker()?.setMaxDate(System.currentTimeMillis())
@@ -78,36 +78,71 @@ class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
         myCalendar.set(Calendar.YEAR, year)
         myCalendar.set(Calendar.MONTH, monthOfYear)
         myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-        myCalendar.add(Calendar.DATE, 0);
+        myCalendar.add(Calendar.DATE, 0)
         // Set the Calendar new date as minimum date of date picker
         //val myFormat = "dd-MMM-yyyy" //In which you need put here
         val myFormat = "yyyy-MM-dd"
         val sdf = SimpleDateFormat(myFormat, Locale.US)
-        dateOfBirthTv.setText(sdf.format(myCalendar.time))
+        dateOfBirthTv.text = sdf.format(myCalendar.time)
     }
     override fun onBackPressed(): Boolean {
         val navController = requireActivity().findNavController(R.id.fragment)
-        if (navController.currentDestination?.id != R.id.fragmentUpdate) {
+        return if (navController.currentDestination?.id != R.id.fragmentUpdate) {
             Log.i("onBackPress", "Not Up Finish All Fragment")
             requireActivity().finish()
-            return true
+            true
         } else {
             Log.i("onBackPress", "Up")
             navController.popBackStack()
-            return true
+            true
         }
+    }
+
+    fun getDataFromFirebase(){
+        val progressDialog =
+                ProgressDialog.show(
+                        activity,
+                        "Please wait",
+                        "Fetching your profile...",
+                        true
+                )
+        mDatabase = FirebaseDatabase.getInstance().getReference("fyproject-6150d")
+                .child("RegisteredUsers").child(firebaseAuth.currentUser!!.uid)
+        mDatabase!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (data in dataSnapshot.children) {
+                    if (data.key == "fName") {
+                        firstNameTv.setText(data.value.toString())
+                    }
+                    if (data.key == "lName") {
+                        lastNameTv.setText(data.value.toString())
+                    }
+                    if (data.key == "mobile") {
+                        mobileTv.setText(data.value.toString())
+                    }
+                    if (data.key == "dateOfBirth") {
+                        dateOfBirthTv.text = data.value.toString()
+                    }
+                    progressDialog.dismiss()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                progressDialog.dismiss()
+            }
+        })
     }
 
     private fun updateDB() {
         try {
             var pwd = etPassword.text.toString().trim()
             var confirmPwd = confirmNewPassword.text.toString().trim()
-            updatePwd(pwd,confirmPwd)
+            updatePwd(pwd, confirmPwd)
         } catch (ex: Exception) {
             Toast.makeText(
-                activity,
-                ex.message,
-                Toast.LENGTH_LONG
+                    activity,
+                    ex.message,
+                    Toast.LENGTH_LONG
             ).show()
         }
     }
@@ -115,51 +150,50 @@ class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
     private fun updatePwd(currPwd: String, newPass: String) {
         val progressDialog =
             ProgressDialog.show(
-                activity,
-                "Please wait",
-                "Checking and updating profile...",
-                true
+                    activity,
+                    "Please wait",
+                    "Checking and updating profile...",
+                    true
             )
-        var user = FirebaseAuth.getInstance().currentUser;
+        var user = FirebaseAuth.getInstance().currentUser
         var credential = EmailAuthProvider
-            .getCredential(sessionManager?.getStringVal(Constant.USER_NAME).toString(), currPwd);
+            .getCredential(sessionManager?.getStringVal(Constant.USER_NAME).toString(), currPwd)
         user?.reauthenticate(credential)
             ?.addOnCompleteListener {
                 if (it.isSuccessful) {
                     user.updatePassword(newPass).addOnCompleteListener {
                         if (it.isSuccessful) {
-                            var firebaseAuth = FirebaseAuth.getInstance()
                             var fName = firstNameTv.text.toString().trim()
                             var lName = lastNameTv.text.toString().trim()
                             var mobile = mobileTv.text.toString().trim()
                             var dateOfBirth = dateOfBirthTv.text.toString().trim()
-                            mDatabase = FirebaseDatabase.getInstance().getReference("fyproject-6150d").child("RegisteredUsers").child(firebaseAuth.currentUser!!.uid);
-                            mDatabase!!.child("fName").setValue(fName);
-                            mDatabase!!.child("lName").setValue(lName);
-                            mDatabase!!.child("mobile").setValue(mobile);
-                            mDatabase!!.child("dateOfBirth").setValue(dateOfBirth);
-                            mDatabase!!.child("password").setValue(newPass);
+                            mDatabase = FirebaseDatabase.getInstance().getReference("fyproject-6150d").child("RegisteredUsers").child(firebaseAuth.currentUser!!.uid)
+                            mDatabase!!.child("fName").setValue(fName)
+                            mDatabase!!.child("lName").setValue(lName)
+                            mDatabase!!.child("mobile").setValue(mobile)
+                            mDatabase!!.child("dateOfBirth").setValue(dateOfBirth)
+                            mDatabase!!.child("password").setValue(newPass)
                             Toast.makeText(
-                                activity,
-                                sessionManager?.getStringVal(Constant.USER_NAME)
-                                    .toString() + ": profile successfully updated!",
-                                Toast.LENGTH_LONG
+                                    activity,
+                                    sessionManager?.getStringVal(Constant.USER_NAME)
+                                            .toString() + ": profile successfully updated!",
+                                    Toast.LENGTH_LONG
                             ).show()
                             findNavController().popBackStack()
                         } else {
                             Toast.makeText(
-                                activity,
-                                it.exception?.message.toString(),
-                                Toast.LENGTH_LONG
+                                    activity,
+                                    it.exception?.message.toString(),
+                                    Toast.LENGTH_LONG
                             ).show()
                         }
-                    };
+                    }
                 } else {
                     Toast.makeText(activity, it.exception?.message.toString(), Toast.LENGTH_LONG)
                         .show()
                 }
                 progressDialog.dismiss()
-            };
+            }
     }
     private fun validation(): Boolean {
         var fName = firstNameTv.text.toString().trim()
@@ -214,8 +248,8 @@ class FragmentUpdate() : Fragment() ,iOnBackPressed, View.OnClickListener {
 
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.updateBtn->{
-                if(validation()){
+            R.id.updateBtn -> {
+                if (validation()) {
                     updateDB()
                 }
             }
